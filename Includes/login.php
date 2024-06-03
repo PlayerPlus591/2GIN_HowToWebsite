42c0d4eb914b9cea2573495da6a65c6ce863974e
PlayerPlus591 <157367332+PlayerPlus591@users.noreply.github.com>
2024-06-03 10:52:37 +0200

    ./CSS/

--- Includes/login.php
+++ Includes/login.php
@@ -17,7 +17,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
-        <link rel="stylesheet" href="RegisterAndLoginStyles.css">
+        <link rel="stylesheet" href="./CSS/RegisterAndLoginStyles.css">
         <link rel="stylesheet" href="https://unpkg.com/98.css">
     </head>
     <body>
