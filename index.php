9a79ba9ee10eb19226c4e804386fdea904b73c72
Awarios <lou200511@gmail.com>
2024-06-03 08:25:44 +0200

    MainPage Update

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>How To Website - Homepage</title>
+        <link rel="stylesheet" href="CSS/mainpage.css">
     </head>
 
     <?php
@@ -11,6 +13,71 @@
     ?>
 
     <body>
-        <h1>Welcum to HowToWebsite</h1>
+        <header>
+            <div class="topnav">
+                <div class="row">
+                    <div class="navleft">
+                        <a class="active" href="index.php">Home</a>
+                        <a href="#server">Server</a>
+                        <a href="#html">HTML</a>
+                        <a href="#php">PHP</a>
+                        <a href="#mysql">MySQL</a>
+                        <a href="#forum">Forum</a>
+                        <a href="#about">About</a>
+                    </div>
+                    <div class="navright">
+                        <a href="Includes/register.php">Sign up</a>
+                        <a href="Includes/login.php">Log in</a>  
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <div class="canvas">
+
+                <h1>Welcome to HowToWebsite</h1>
+                
+                <div class="card">
+                    <h2>Main Description</h2>
+                    <p>When making a website, you might come across a few problems that you can not seem to solve.</p>
+                    <p>Here you will learn Step By Step how to make your own website. You will learn how to set up your server and how to
+                    use HTML, CSS, JavaScript and more.</p>
+                </div>
+
+                <div class="row">
+
+                    <div class="column">
+                        <div class="card">
+                            <h2>Overview</h2>
+                            <ol>
+                                <li><p>Ubuntu Server Installation</p></li>
+                                <li><p>Ubuntu Server Setup</p></li>
+                                <li><p>Visual Studio Code x Server Connection</p></li>
+                                <li><p>GitHub x VS Code Connection</p></li>
+                                <li><p>Hardcoding your first html page</p></li>
+                            </ol>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="card">
+                            <h2>Requirements</h2>
+                            <ul>
+                                <li><p>PC</p></li>
+                                <li><p>(Server if you host from home)</p></li>
+                                <li><p>Oracle VM VirtualBox</p></li>
+                                <li><p>Ubuntu</p></li>
+                                <li><p>Visual Studio Code</p></li>
+                                <li><p>GitHub</p></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </main>
     </body>
 </html>
