3409219cd984c06ef8d0ce1f5835b2a05a275674
PlayerPlus591 <psplusplayer591@gmail.com>
2024-06-08 20:42:56 +0200

    Update database.php

--- Includes/database.php
+++ Includes/database.php
@@ -1,14 +1,14 @@
 <!--Pedro Code-->
 
 <?php
+    $hostName= "localhost";
+    $dbUser = "H2W_User";
+    $dbPassword = "h2wpw";
+    $dbName = "dbHow2Website";
 
-$hostName= "localhost";
-$dbUser = "H2W_User";
-$dbPassword = "h2wpw";
-$dbName = "dbHow2Website";
-//connection sends a true or false
-$connection = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-if(!$connection){
-    die("Something went wrong");
-}
+    //connection sends a true or false
+    $connection = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+    if(!$connection){
+        die("Something went wrong");
+    }
 ?>
