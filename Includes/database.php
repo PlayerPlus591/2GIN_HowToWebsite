ba909837fb669703648ae3c0edac9757538a004b
PedroEL420 <157367574+PedroEL420@users.noreply.github.com>
2024-06-10 21:52:01 +0200

    Made forum
    Forum is fully implemented with the project, made a separate database for the informations. BUGS: when refreshing the page after making a comment it duplicates itself

--- Includes/database.php
+++ Includes/database.php
@@ -1,14 +1,16 @@
 <!--Pedro Code-->
 
 <?php
-    $hostName= "89.58.47.144";
-    $dbUser = "H2W_User";
-    $dbPassword = "h2wpw";
-    $dbName = "dbHow2Website";
+$hostName = "89.58.47.144";
+$dbUser = "H2W_User";
+$dbPassword = "h2wpw";
+$dbName = "dbHow2Website";
 
-    //connection sends a true or false
-    $connection = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
-    if(!$connection){
-        die("Something went wrong");
-    }
+// Establish a connection to the database
+$connection = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+
+// Check if the connection was successful
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
---
+++ Includes/reply.php
@@ -0,0 +1,23 @@
+<!--Filipe-->
+<?php if (isset($reply_data)): ?>
+<div class="reply">
+  <h4><?php echo htmlspecialchars($reply_data['name'], ENT_QUOTES, 'UTF-8'); ?></h4>
+  <p><?php echo htmlspecialchars($reply_data['date'], ENT_QUOTES, 'UTF-8'); ?></p>
+  <p><?php echo htmlspecialchars($reply_data['comment'], ENT_QUOTES, 'UTF-8'); ?></p>
+  <?php $nested_reply_id = $reply_data['id']; ?>
+  <button class="reply" onclick="reply(<?php echo $nested_reply_id; ?>, '<?php echo htmlspecialchars($reply_data['name'], ENT_QUOTES, 'UTF-8'); ?>');">Reply</button>
+  
+  <?php
+  // Fetch and display nested replies
+  $nested_replies = mysqli_query($connection, "SELECT * FROM tb_data WHERE reply_id = $nested_reply_id");
+  if (mysqli_num_rows($nested_replies) > 0) {
+    echo '<div class="replies">';
+    while ($nested_reply_data = mysqli_fetch_assoc($nested_replies)) {
+      // Include the reply.php to render nested replies
+      include 'reply.php';
+    }
+    echo '</div>';
+  }
+  ?>
+
+
