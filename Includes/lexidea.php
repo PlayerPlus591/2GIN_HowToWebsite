81c82d21956c4c80f18c3898ac7ab12159ef9572
alethi07 <157472584+alethi07@users.noreply.github.com>
2024-06-24 19:18:31 +0200

    Update lexidea.php

--- Includes/lexidea.php
+++ Includes/lexidea.php
@@ -53,6 +53,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../CSS/cours.css">
         <title>How to Website</title>
+        <script src="../JS/mobileMenu.js"></script>
     </head>
     <?php
     ?>
