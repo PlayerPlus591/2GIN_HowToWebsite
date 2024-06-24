d4f261511f2a3c29836c12084fc787d8de5730da
alethi07 <157472584+alethi07@users.noreply.github.com>
2024-06-24 19:18:02 +0200

    Update templatedot.php

--- Includes/templatedot.php
+++ Includes/templatedot.php
@@ -259,19 +259,7 @@
             }
         }
 
-        function getRandomColor() {
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
-
-        function changeColor() {
-            const square = document.getElementById('colorSquare');
-            square.style.backgroundColor = getRandomColor();
-        }
+        
 
         setInterval(changeColor, 100); 
     </script>
